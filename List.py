ceb0883cc5897d3559948ffd526df47970755748
minji12344 <alswl12344@naver.com>
2023-07-25 11:16:26 +0900

    코드구분

--- List.py
+++ List.py
@@ -1,5 +1,6 @@
 x = [1, 6, 4, 3, 7, 2, 9]
 
+# 최소값
 for i in range(len(x)):
     for j in range(i+1,len(x)):
         if x[i] > x[j]:
@@ -15,6 +16,12 @@ print(f"갯수 : {len(x)}")
 print(f"최소값 : {x[0]}")
 print(f"최대값 : {x[a]}")
 
+# 최대값
 
+
+# 합계
+
+
+# 평균
 
 
