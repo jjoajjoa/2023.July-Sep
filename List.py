07950094a337a4d959ffa12f68d0cf77b267297d
Arum <rlfehd429@gmail.com>
2023-07-25 10:41:59 +0900

    오름차순

--- List.py
+++ List.py
@@ -1,2 +1,10 @@
 x = [1, 6, 4, 3, 7, 2, 9]
+
+for i in range(len(x)):
+    for j in range(i+1,len(x)):
+        if x[i] > x[j]:
+            temp = x[i]
+            x[i] = x[j]
+            x[j] = temp
+
 print(x)
