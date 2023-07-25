74112afc66780f51285a401b196ac3009920af63
Arum <rlfehd429@gmail.com>
2023-07-25 12:52:23 +0900

    코드 엉켜놓기

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
 x = [1, 6, 4, 3, 7, 2, 9]
 
-# 최소값
+
 for i in range(len(x)):
     for j in range(i+1,len(x)):
         if x[i] > x[j]:
@@ -13,15 +13,23 @@ n = int(len(x))
 a = n-1
 
 print(f"갯수 : {len(x)}")
+
+
+# 최소값, 최대값
 print(f"최소값 : {x[0]}")
 print(f"최대값 : {x[a]}")
 
-# 최대값
-
 
 # 합계
+## print(x)
+nums = 0
+for summ in x:
+    nums += summ
+
+print(f"합계 : {nums}")
 
 
 # 평균
+print(f"평균 : {int(nums/n)}")
 
 
