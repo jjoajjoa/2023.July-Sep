64854000a5b7f9d9b1c6e2371da7b39d17ecf666
jjoajjoa <rlfehd429@gmail.com>
2023-07-30 22:44:49 +0900

    0730 2244

---
+++ Study02/src/homework/Hw01.java
@@ -0,0 +1,48 @@
+package homework;
+
+public class Hw01 {
+
+	public static void main(String[] args) {
+
+
+		// * 트리 만들기 (앞에서부터)
+		
+		// 01
+//		String a = "*";
+//		System.out.println(a);		
+//		for (int i=0; i<4; i++) {			
+//			a = a + "*";			
+//			System.out.println(a);
+//		}	
+		
+		// 02
+//		String a = "*";
+//		for (int i=0; i<5; i++) {			
+//			System.out.println(a);
+//			a = a + "*";			
+//		}
+		
+		
+		
+		// * 트리 만들기 (앞에서부터 -> 역순): 배열사용	
+		
+		String [] array = new String[5];	
+		
+		array[0] = "*";	
+		System.out.println(array[0]);
+		
+		for (int i=1; i<5; i++) {
+			array[i] = array[i-1] + "*";
+			System.out.println(array[i]);
+		}
+		
+		
+		for (int i=4; i>=0; i--) {
+			System.out.println(array[i]);
+		}	
+			
+			
+		
+	}
+
+}
---
+++ Study02/src/homework/Hw02.java
@@ -0,0 +1,77 @@
+package homework;
+
+public class Hw02 {
+
+	public static void main(String[] args) {
+		
+		
+		// * 트리 만들기 (뒤에서부터)
+		
+		String [] arr = new String[5];
+			
+//		arr[0] = 4spa 1sta
+//		arr[1] = 3spa 2sta
+//		arr[2] = 2spa 3sta
+//		arr[3] = 1spa 4sta
+//		arr[4] = 	  5sta
+//				   j    i    
+				
+	
+		int maxIndex = 100;
+		
+		String[] starArr = new String[maxIndex];
+		String[] spaceArr = new String[maxIndex];
+		
+		String star = "*";
+		String space = " ";
+		
+//		for(int i = 0; i < maxIndex; i++) {
+//			starArr[i] = star;
+//			star += "*";
+//		}
+//		
+//		for(int i = maxIndex-1; i >= 0; i--) {
+//			spaceArr[i] = space;
+//			space += " ";
+//		}
+//		
+//		for(int i = 0; i < maxIndex; i++) {
+//			System.out.println(spaceArr[i]+starArr[i]);
+//		}
+
+		
+		
+		for (int i=1,j=maxIndex-1; i<=maxIndex&&j>=0; i++,j--) {
+			
+			
+			String spaceResult = "";
+			String starResult = "";
+			
+			if(j > 0) {				
+				for(int x=0; x < j; x++) {
+					spaceResult += space; 
+				}
+			}
+
+			for(int x=0; x < i; x++) {
+				starResult += star;
+			}
+			
+			System.out.println(spaceResult + starResult);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+}
