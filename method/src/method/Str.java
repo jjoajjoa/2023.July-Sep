2e2fd0455605b75211abcd767aa6edf2dc824133
Arum <rlfehd429@gmail.com>
2023-07-31 18:35:05 +0900

    0731 1835

--- method/src/method/Str.java
+++ method/src/method/Str.java
@@ -18,9 +18,19 @@ public class Str {
 		System.out.println(sentence.substring(2,9)); // lloProf :: substring(a,b) -> a부터 b 전까지
 		
 		System.out.println(sentence.charAt(4)); // o
-		System.out.println(sentence.indexOf("f")); // 8
+		System.out.println(sentence.indexOf("l")); // 2 : 문자열에서 가장 앞에있는 애
 		System.out.println(sentence.indexOf("p")); // -1
-
+		System.out.println(sentence.lastIndexOf("l")); // 3 : 가장 뒤에있는 애
+		
+		System.out.println(greeting.concat(sentence)); // HelloHelloProfessor
+		System.out.println(greeting.equals("Hello")); // true
+		System.out.println(greeting.equals("hello")); // false
+		System.out.println(greeting.equalsIgnoreCase("hello")); // true :: 대소문자 구분 X
+		System.out.println(greeting.compareTo("Aello")); // 7   :: H보다 A가 7번째 앞에있음
+		System.out.println(greeting.compareTo("Hello")); // 0   ::
+		System.out.println(greeting.compareTo("Hezzo")); // -14 :: l보다 z가 14번째 뒤에있음
+		
+		
 	}
 
 }
