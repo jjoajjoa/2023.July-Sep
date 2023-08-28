8d83f9db506acafb8cbb8ce5baa1e3875781bd3d
Arum <rlfehd429@gmail.com>
2023-08-28 18:19:24 +0900

    0828 1819

--- Graph/Study03.py
+++ Graph/Study03.py
@@ -7,8 +7,6 @@ import numpy as np
 mpl.rcParams["font.family"] = "Malgun Gothic"
 mpl.rcParams["axes.unicode_minus"] = False
 
-# df = pd.read_csv("seocho_people.csv")
-# print(df)
 
 def read_csv_file(file_path):
     data = []
@@ -33,7 +31,6 @@ def create_scatter_plot(data):
     plt.xlabel("나이")
     plt.title("서초구 사람들")
 
-
 def compare_age(data):
     male_lst = [age for age, gender, _ in data if gender == "남성"]
     female_lst = [age for age, gender, _ in data if gender == "여성"]
@@ -46,34 +43,29 @@ def compare_age(data):
 
 
 
-data_set = read_csv_file("seocho_people.csv")
+# 어캐하지
 
-create_scatter_plot(data_set)
+print("서초사람들 요약 >>> ", read_csv_file("seocho_people.csv"))
 
-plt.colorbar(label="Gender")
-plt.show()
-compare_age(data_set)
+lst_man = []
+lst_man_add = []
+lst_woman = []
+
+for i in read_csv_file("seocho_people.csv"):
+    if i[1] == "남성":
+        lst_man.append(i[0])
+        lst_man_add.append(i[2])
+    else:
+        lst_man.append(np.nan)
+        lst_man_add.append(np.nan)
+# plt.scatter(lst_man_add, lst_man)
+# plt.show()
+# print(len(lst_man))
+# print(len(lst_man_add))
+print(lst_man)
+print(lst_man_add)
 
 
-
-
-
-
-
-# print("서초사람들 요약 >>> ", read_csv_file("seocho_people.csv"))
-# lst_man = []
-# lst_woman = []
-# # 1. 나이와 성별에 따른 산점도 그래프 그리기
-# # 2. 성별 평균 나이 구하기
-#
-# # 그래프 그리기
-#
-# for i in read_csv_file("seocho_people.csv"):
-#     if i[1] == "남성":
-#         lst_man.append(i[0])
-#     else:
-#         lst_man.append(np.nan)
-#
 # for i in read_csv_file("seocho_people.csv"):
 #     if i[1] == "여성":
 #         lst_woman.append(i[0])
@@ -81,19 +73,19 @@ compare_age(data_set)
 #         lst_woman.append(np.nan)
 #
 # total = [i*0 for i in range(len(read_csv_file("seocho_people.csv")))]
-#
-# print(len(lst_man))
+
+# print(lst_man)
+# print(lst_man_add)
 # print(len(lst_woman))
 # print(total)
-#
-# plt.ylim("man", "woman")
-# plt.title("seocho people")
-#
-# # plt.scatter(lst_man,total, lst_woman,total)
+
+
+
+# plt.scatter(lst_man,total, lst_woman,total)
 # plt.scatter(lst_man, total,10,"b")
-# # plt.scatter(lst_woman, total,15,"r")
-#
-# # plt.plot(total,lst_man, total,lst_woman)
+# plt.scatter(lst_woman, total,15,"r")
+
+# plt.plot(total,lst_man, total,lst_woman)
 # plt.show()
 
 
@@ -102,12 +94,13 @@ compare_age(data_set)
 
 
 
-
-
-
-
-
-
+# data_set = read_csv_file("seocho_people.csv")
+#
+# create_scatter_plot(data_set)
+#
+# plt.colorbar(label="Gender")
+# plt.show()
+# compare_age(data_set)
 
 
 
